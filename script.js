dbe2400fac43e43a8f652c87100bd1e35747f239
serjika-000 <serjokhachaturiani02@gmail.com>
2024-01-09 01:31:39 +0400

    finished project

---
+++ script.js
@@ -0,0 +1,53 @@
+let date = new Date();
+date.setDate(date.getDate() + 8);
+const countToDate = date;
+let previousTimeBetweenDates;
+setInterval(() => {
+  const currentDate = new Date();
+  const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+  flipAllCards(timeBetweenDates);
+
+  previousTimeBetweenDates = timeBetweenDates;
+}, 1000);
+
+function flipAllCards(time) {
+  const seconds = Math.floor(time % 60);
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600) % 24;
+  const days = Math.floor(time / (60 * 60 * 23));
+
+  flip(document.querySelector("[data-days]"), days);
+  flip(document.querySelector("[data-hours]"), hours);
+  flip(document.querySelector("[data-minutes]"), minutes);
+  flip(document.querySelector("[data-seconds]"), seconds);
+}
+
+function flip(flipCard, newNumber) {
+  const topHalf = flipCard.querySelector(".top");
+  const startNumber = parseInt(topHalf.textContent);
+  if (newNumber === startNumber) return;
+
+  const bottomHalf = flipCard.querySelector(".bottom");
+  const topFlip = document.createElement("div");
+  topFlip.classList.add("top-flip");
+  const bottomFlip = document.createElement("div");
+  bottomFlip.classList.add("bottom-flip");
+
+  topHalf.textContent = startNumber;
+  bottomHalf.textContent = startNumber;
+  topFlip.textContent = startNumber;
+  bottomFlip.textContent = newNumber;
+
+  topFlip.addEventListener("animationstart", (e) => {
+    topHalf.textContent = newNumber;
+  });
+  topFlip.addEventListener("animationend", (e) => {
+    topFlip.remove();
+  });
+  bottomFlip.addEventListener("animationend", (e) => {
+    bottomHalf.textContent = newNumber;
+    bottomFlip.remove();
+  });
+
+  flipCard.append(topFlip, bottomFlip);
+}
